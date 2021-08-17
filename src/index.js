6acddbbd8b190e52c7fe6bc388716efdcfcb2a58
kendo069960 <kendo069960@gmail.com>
2021-08-17 21:28:10 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const route = require('./routes')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const route = require('./routes');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({extname: '.hbs'}))
-app.set('view engine', 'hbs')
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 // change directory of views folder
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app)
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
-const newRouter = require('./news')
-const siteRouter = require('./site')
+const newRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app) {
-    
-    app.use('/news', newRouter)
-    app.use('/', siteRouter)
+    app.use('/news', newRouter);
+    app.use('/', siteRouter);
 
     // app.get('/', (req, res) => {
     //     res.render('home')
@@ -19,4 +18,4 @@ function route(app) {
     // })
 }
 
-module.exports = route
+module.exports = route;
